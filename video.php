713cc81142101333ad37a9c87a0526ac9722f3e1
Rudi Ponthier <rudi.ponthier@telenet.be>
2019-01-24 09:41:58 +0100

    Test video.php eerste video gekopieerd

--- video.php
+++ video.php
@@ -24,51 +24,53 @@ $description="Liveopnames van de Belgische rootsrockband Paralyzers (Genk on sta
           </div>
         </section>
 
-        <h2 class="youtube">Hookrock Indoor 2015</h2>
+        <h2 class="youtube">Genk on stage 2013</h2>
         <section class="paralyzersvideo">
           <div class="youtubevideo">
             <div class="videowrapper">
-              <iframe src="https://www.youtube.com/embed/W1GTyTIgwlE?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live op Hookrock Indoor 2015 - Twist of the knife"></iframe>
+              <iframe src="http://www.youtube.com/embed/eFWvQL_9Q6M?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers op Genk on stage 2013 - Come on, Teenage kicks en Lonely boy"></iframe>
               </div>
           </div>
           <div class="youtubetekst">
-            <p>Op de uitgave 2015 van Hookrock Indoor openden de Paralyzers voor Barrelhouse en de Finse Ina Forsman Band. Voor de eerste keer in 17 jaar stonden de Paralyzers in hun originele bezetting op het podium: samen met mede-oprichter Marco brachten de Paralyzers "Twist of the knife" van the Fabulous Thunderbirds.</p>
+            <p>In juni 2013 speelden de Paralyzers voor de vierde keer op Genk on stage.</p>
           </div>
         </section>
 
-        <h2 class="youtube">Hookrock Indoor 2015</h2>
+        <h2 class="youtube">Clash of the coverbands in Genk 2012</h2>
         <section class="paralyzersvideo">
           <div class="youtubevideo">
             <div class="videowrapper">
-              <iframe src="https://www.youtube.com/embed/W1GTyTIgwlE?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live op Hookrock Indoor 2015 - Twist of the knife"></iframe>
+              <iframe src="http://www.youtube.com/embed/OtL4B2xUc1s?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live in Genk 2012 - Teenage kicks, Lonely boy en The KKK took my baby away"></iframe>
               </div>
           </div>
           <div class="youtubetekst">
-            <p>Op de uitgave 2015 van Hookrock Indoor openden de Paralyzers voor Barrelhouse en de Finse Ina Forsman Band. Voor de eerste keer in 17 jaar stonden de Paralyzers in hun originele bezetting op het podium: samen met mede-oprichter Marco brachten de Paralyzers "Twist of the knife" van the Fabulous Thunderbirds.</p>
+            <p>Voor de grap schreven de Paralyzers zich in 2012 in voor de wedstrijd Clash of the Coverbands. Plots stonden ze in de regiofinale zuid België/Luxemburg. 
+            In de finale van het optreden speelden ze "Teenage kicks" van de Undertones, "Lonely boy" van de Black Keys en "The KKK took my baby away" van de Ramones.</p>
           </div>
         </section>
 
-        <h2 class="youtube">Hookrock Indoor 2015</h2>
+        <h2 class="youtube">Blues in Genk 2010</h2>
         <section class="paralyzersvideo">
           <div class="youtubevideo">
             <div class="videowrapper">
-              <iframe src="https://www.youtube.com/embed/W1GTyTIgwlE?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live op Hookrock Indoor 2015 - Twist of the knife"></iframe>
+              <iframe src="http://www.youtube.com/embed/SsxllaeDXC4?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live op Blues in Genk 2010 - Shadow play"></iframe>
               </div>
           </div>
           <div class="youtubetekst">
-            <p>Op de uitgave 2015 van Hookrock Indoor openden de Paralyzers voor Barrelhouse en de Finse Ina Forsman Band. Voor de eerste keer in 17 jaar stonden de Paralyzers in hun originele bezetting op het podium: samen met mede-oprichter Marco brachten de Paralyzers "Twist of the knife" van the Fabulous Thunderbirds.</p>
+            <p>Een stukje "Shadow play" van de grote Rory Gallagher op het bluesfestival in Genk in 2010.</p>
           </div>
         </section>
 
-        <h2 class="youtube">Hookrock Indoor 2015</h2>
+        <h2 class="youtube">Muziekodroom Hasselt 2002</h2>
         <section class="paralyzersvideo">
           <div class="youtubevideo">
             <div class="videowrapper">
-              <iframe src="https://www.youtube.com/embed/W1GTyTIgwlE?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live op Hookrock Indoor 2015 - Twist of the knife"></iframe>
+              <iframe src="http://www.youtube.com/embed/GOr39gX-lJc?feature=player_detailpage&amp;wmode=transparent" frameborder="0" allowfullscreen title="The Paralyzers live in Muziekodroom Hasselt 2002 - Mumbo jumbo"></iframe>
               </div>
           </div>
           <div class="youtubetekst">
-            <p>Op de uitgave 2015 van Hookrock Indoor openden de Paralyzers voor Barrelhouse en de Finse Ina Forsman Band. Voor de eerste keer in 17 jaar stonden de Paralyzers in hun originele bezetting op het podium: samen met mede-oprichter Marco brachten de Paralyzers "Twist of the knife" van the Fabulous Thunderbirds.</p>
+            <p>In 2002 verzorgden de Paralyzers in de Muziekodroom in Hasselt het voorprogramma van Jim Suhler. 
+              Onder andere "Mumbo jumbo" van de Tail Gators passeerde de revue.</p>
           </div>
         </section>
 
