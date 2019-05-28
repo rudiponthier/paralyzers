031422d81dae45495578bb4a6a08cfeddb227208
Rudi Ponthier <rudi.ponthier@telenet.be>
2019-05-28 15:48:49 +0200

    Optreden Pinkblues in Someren geannuleerd

--- live.php
+++ live.php
@@ -24,9 +24,7 @@ $description="Optredens van rootsrockband Paralyzers.";
 	          <div style="display: block">
 	            <ul>
 	              <li>
-	              	<h3 class="gig-date">ma 10 juni 2019, 18h</h3>
-	              	<span class="gig-city">Someren (NL)</span>
-	              	<span class="gig-name">Pinkblues, Café Zaal Brasserie "<a href="https://indenherberg.nl" target ="_blank">In d'n Herberg</a>", Postel 27, 5711 ET Someren</span>
+	              	<p>Het geplande optreden van maandag 10 juni op Pinkblues in Someren (NL) kan helaas wegens ziekte niet doorgaan.<br />Voorlopig zijn er geen andere optredens gepland.</p>
 	              </li>
 	            </ul>
 	          </div>
